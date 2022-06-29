dd039f848e6dc8daa403b6346b3c4cae065d2fee
luis rosero <luis.rosero@wiga.io>
2022-06-29 12:36:35 -0500

    Second Commit

--- Controllers/AutoresController.cs
+++ Controllers/AutoresController.cs
@@ -15,14 +15,44 @@ namespace WebApiAutores.Controllers
             this.context = context;
         }
 
-        [HttpGet]
+        [HttpGet] 
+        // [HttpGet("listado")] api/autores/listado
+        // [HttpGet("/listado")] listado "remplaza la ruta principal"
         public async Task<ActionResult<List<Autor>>> Get()
         {
             return await context.Autores.Include(x => x.Libros).ToListAsync();
         }
 
+        [HttpGet("primero")]
+        public async Task<ActionResult<Autor>> PrimerAutor([FromHeader] int miValor, [FromQuery] string nombre)
+        {
+            return await context.Autores.FirstOrDefaultAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Autor>> Get(int id)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            return autor;
+        }
+
+        [HttpGet("{nombre}")]
+        public async Task<ActionResult<Autor>> Get([FromRoute] string nombre)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Name == nombre);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            return autor;
+        }
+
         [HttpPost]
-        public async Task<ActionResult> Post(Autor autor) 
+        public async Task<ActionResult> Post([FromBody] Autor autor) 
         {
             context.Add(autor);
             await context.SaveChangesAsync();
@@ -50,6 +80,11 @@ namespace WebApiAutores.Controllers
         }
 
         [HttpDelete("{id:int}")]
+        // [HttpDelete("{id:int}/{param2}")] "Doble parametro"
+        // [HttpDelete("{id:int}/{param2?}")] "Doble parametro opcional"
+        // [HttpDelete("{id:int}/{param2 = valor}")] "Doble parametro por defecto"
+
+
         public async Task<ActionResult> Delete(int id)
         {
             var existe = await context.Autores.AnyAsync(x => x.Id == id);
--- Entidades/Autor.cs
+++ Entidades/Autor.cs
@@ -1,9 +1,24 @@
-﻿namespace WebApiAutores.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiAutores.Entidades
 {
     public class Autor
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requirido")]
+        [StringLength(5, ErrorMessage ="El campo {0} no puede tener mas de {1} caracteres")]
         public string Name { get; set; }
+
+        [Range(18,25)]
+        [NotMapped]
+        public int edad { get; set; }
+
+        [CreditCard]
+        [NotMapped]
+        public string tarjetaDeCredito { get; set; }
+
         public List<Libro> Libros { get; set; }
 
     }
